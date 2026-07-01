eec9262262f788d83bc609ca20951e25d8499ac6
hug0b <shbayet@gmail.com>
2026-07-01 23:45:37 +0200

    add linktree icon and fix avatar responsiveness

---
+++ src/components/SvgIcons/LinktreeIcon.tsx
@@ -0,0 +1,12 @@
+import { SvgIcon, type SvgIconProps } from "@mui/material";
+
+/**
+ * Linktree icon for the sidebar
+ */
+export function LinktreeIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox="0 0 24 24" {...props}>
+      <path d="M13.736 5.853l4.005-4.117 2.325 2.38-4.2 4.005h5.908v3.305h-5.937l4.229 4.108-2.325 2.334-5.74-5.769-5.741 5.769-2.325-2.325 4.229-4.108H2.226V8.121h5.909l-4.2-4.004 2.324-2.381 4.005 4.117V0h3.472zM10.264 15.089h3.472V24h-3.472z" />
+    </SvgIcon>
+  );
+}
