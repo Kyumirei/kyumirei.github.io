f5df6952235bb31c7ffbbf6a5ff7560ab6eaa0c8
Kyumirei <epelloquin2006@gmail.com>
2026-07-02 20:29:17 +0200

    Changement texte (test)
    Changement texte (test)

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -35,10 +35,21 @@ export default function AboutPage() {
           textAlign: { xs: "left", md: "justify" }
         }}
       >
-        Cette grande aventure a commencé au début du collège, alors que je me suis prise de passion pour les mangas que je lisais dans la petite bibliothèque de mon école. J'ai
-        adoré ce style de dessin. Depuis je m'accroche à ce rêve si fou de pouvoir un jour devenir mangaka ou illustratrice. Mais le chemin n'est pas encore fini... Dans cet
-        objectif, j'ai rejoint une école de manga et d'illustration à Toulouse depuis 2024, et je poursuis désormais mon apprentissage en vue de devenir professionnelle et d'être
-        fière de mon travail.
+        Je suis une artiste, illustratrice, concept art, et aspirante mangaka.
+
+Cette grande aventure a commencé au collège, lorsque je me suis découvert une véritable passion pour les mangas que je dévorais dans la petite bibliothèque de mon établissement. Fascinée par leur univers et leur style graphique, j'ai très vite commencé à rêver de créer les miens.
+
+Pour moi, le dessin est bien plus qu'une discipline artistique : c'est un langage. Chaque illustration, chaque personnage et chaque projet est une occasion d'explorer mon imagination, de raconter des histoires et de transmettre des émotions. Je suis convaincue que l'art a le pouvoir de faire rêver, d'inspirer, de faire réfléchir et, parfois, d'aider une personne à avancer dans sa propre vie à travers les récits et les images qui les accompagnent.
+
+Je puise également mon inspiration dans la philosophie, la psychologie et le développement personnel. Comprendre l'être humain, ses émotions et sa manière d'appréhender le monde nourrit ma créativité et influence profondément les univers que je construis.
+
+
+Depuis, je poursuis ce rêve un peu fou : devenir un jour artiste professionnelle. Mon ambition est de travailler comme illustratrice, mangaka, ou bien encore concept art spécialisé dans les décors (background artist) pour le jeu vidéo ou l'animation. Le chemin est encore long, mais chaque je me rapproche petit à petit de cet objectif. 
+
+Depuis 2024, j'ai intégré une école de manga et d'illustration à Toulouse, où je continue à apprendre, à progresser et à développer mon univers afin de vivre un jour de ma passion et d'être fière de mon travail.
+
+Si vous souhaitez suivre mon aventure, découvrir mes créations ou même me soutenir, ce sera avec un immense plaisir ! Votre soutien, qu'il s'agisse d'un simple regard, d'un commentaire ou d'un partage, m'encourage à continuer à créer et à poursuivre ce rêve qui m'anime depuis tant d'années. ♥
+
       </Typography>
 
       <Typography
@@ -49,7 +60,7 @@ export default function AboutPage() {
           mb: { xs: 3, md: 4 }
         }}
       >
-        Si vous souhaitez suivre mon aventure, et même me soutenir, c'est avec un grand plaisir !
+    test
       </Typography>
 
       <Box
